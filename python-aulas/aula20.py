3d04d3998575696dff65a09390be0982a676b1eb
Pedro Silva Novais <pedrosilvanovais7@gmail.com>
2025-03-10 17:30:18 -0300

    Update aula20.py
    Muitas coisas aprendidas na aula de Funções (parte1)

--- python-aulas/aula20.py
+++ python-aulas/aula20.py
@@ -1,6 +1,43 @@
-def desafio(num):
-    print(f'{f" Desafio {num} ":-^40}')
+#funcão sem parametro
+def linha():
+    print('-' * 30)
 
 
-desafio(90)
-desafio(18)
+# função com parametro
+def soma(a, b):
+    print(f'A = {a} \nB = {b}')
+    s = a + b
+    print(f'A soma de A + B é igual a = {s}')
+
+
+# desempacotamento
+def contador(*num):
+    tam = len(num)
+    print(f'Recebi os valores {num} que são ao todo {tam} numeros')
+
+
+# usando listas
+def dobra(lista):
+    pos = 0
+    while pos < len(lista):
+        lista[pos] *= 2
+        pos += 1
+
+
+# Programa principal
+linha()
+print('Aula 20')
+linha()
+
+soma(b=9, a=6)
+soma(4, 8)
+linha()
+
+contador(2, 5, 6, 9)
+contador(6, 9, 5, 8, 9, )
+linha()
+
+valores = [9, 8, 5, 6, 1]
+print(valores)
+dobra(valores)
+print(valores)
