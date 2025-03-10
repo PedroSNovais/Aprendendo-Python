459fd7fba055542e2b516d70181d4584c90f8df4
Pedro Silva Novais <pedrosilvanovais7@gmail.com>
2025-03-10 11:05:23 -0300

    Update desafio95.py

--- python-exercicios/desafio95.py
+++ python-exercicios/desafio95.py
@@ -9,6 +9,7 @@ while True:
     print(f'{"CADASTRE O JOGADDOR":^40}')
     jogador['nome'] = str(input('Nome: ').strip().capitalize())
     partidas = int(input('Quantas partidas ele jogou ? '))
+    jogador['partidas'] = partidas
     for p in range(0, partidas):
         gols.append(int(input(f'fez quantos gols na partida {p}? ')))
         tot += gols[p]
@@ -20,7 +21,20 @@ while True:
     if resp == 'N':
         break
 print('--' * 20)
-print(f'{"Nº":<10}{"NOME"} {"TOTAL DE GOLS":>20}')
+print(f'{"codº":<10}{"NOME"} {"TOTAL DE GOLS":>20}')
 print('-' * 30)
 for n, j in enumerate(time):
     print(f'{n:.<10}{j["nome"]:.<15} {j["total"]}')
+print('-=' * 30)
+while True:
+    n = (int(input('Digite o codº do jogador para ver seu desempenho: ( 999 = parar ) ')))
+    if n == 999:
+        break
+    elif n >= len(time):
+        print(f'O jogador com o codº {n} não foi cadastrado... ')
+    else:
+        print(f'O jogador "{time[n]["nome"]}" jogou {time[n]["partidas"]}.')
+        for p in range(0, time[n]['partidas']):
+            print(f' => Na partida {p}, fez {time[n]["gols"][p]} gols')
+        print(f'No total foram {time[n]["total"]} gols')
+print(f'<<< FIM DO PROGRAMA >>>')
