f0bf2e3d9bdfd4d8a20938295394f81c5df89d61
Pedro Silva Novais <pedrosilvanovais7@gmail.com>
2025-03-11 18:37:52 -0300

    coisas

---
+++ python-exercicios/desafio106.py
@@ -0,0 +1,30 @@
+from teste import titulo
+
+
+def txtlinha(msg):
+    print('~' * (len(msg) + 4))
+    print(f'  {msg}')
+    print('~' * (len(msg) + 4))
+
+
+def cor(cod=''):
+    print(f'\033[{cod}m')
+
+
+# programa principal
+titulo('Desafio 106')
+while True:
+    cor('42')
+    txtlinha('Mini-Sistema de ajuda PyHelp')
+    cor()
+    cor('31')
+    print('Digite "FIM" para finalizar o programa !!!')
+    codigo = input('Digite um codigo Python para ver seu Docstring: ')
+    cor()
+    if codigo == 'FIM':
+        cor('41;30')
+        txtlinha('<<<  O PROGRAMA FOI FINALIZADO >>>')
+        break
+    cor('1;47;30')
+    help(codigo)
+    cor(0)
