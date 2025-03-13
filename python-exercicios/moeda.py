c64f17c1d70251b85a97acb68d0ceed57db7f6c1
Pedro Silva Novais <136607171+PedroSNovais@users.noreply.github.com>
2025-03-13 16:58:46 -0300

    Update moeda.py
    add atividade do desafio 108

--- python-exercicios/moeda.py
+++ python-exercicios/moeda.py
@@ -1,23 +1,31 @@
-print(f'{" Desafio 107 ":*^40}')
+print(f'{" Desafio 107/8 ":*^40}')
 
 
 def dobro(n):
-    n *= 2
-    return n
+    t = n
+    t *= 2
+    return t
 
 
 def metade(n):
-    t = int(n)
+    t = n
     t = t / 2
     return t
 
 
 def aumentar(n, pc=10):
-    t = int(n)
+    t = n
     t += (t * pc) / 100
     return t
 
 def diminuir(n, pc=15):
-    t = int(n)
+    t = n
     t -= (t * pc) / 100
     return t
+
+
+# desafio 108
+def moeda(msg):
+    msg = int(msg)
+    msg = f'R${msg}.00'
+    return msg
