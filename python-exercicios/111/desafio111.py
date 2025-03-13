aebedf8269f6594970d56ad07ac4e7d6ee3d727c
Pedro Silva Novais <136607171+PedroSNovais@users.noreply.github.com>
2025-03-13 19:46:40 -0300

    desafio 111
    criação de pacotes dentro do pacote "utilidadesCEV"

---
+++ python-exercicios/111/desafio111.py
@@ -0,0 +1,3 @@
+from utilidadesCEV import moeda
+p = float(input('Digite um valor: R$'))
+moeda.resumo(p, 80, 25)
---
+++ python-exercicios/111/utilidadesCEV/moeda/__init__.py
@@ -0,0 +1,38 @@
+print(f'{" Desafio 107/8/9/10/11 ":*^40}')
+
+
+def dobro(valor, formato=False):
+    valor *= 2
+    return valor if not formato else moeda(valor)
+
+
+def metade(valor, formato=False):
+    valor = valor / 2
+    return valor if not formato else moeda(valor)
+
+
+def aumentar(valor, pcento=10, formato=False):
+    valor += (valor * pcento) / 100
+    return valor if not formato else moeda(valor)
+
+def diminuir(valor, pcento=15, formato=False):
+    valor -= (valor * pcento) / 100
+    return valor if not formato else moeda(valor)
+
+
+def moeda(msg, moeda='R$'):
+    msg = f'{moeda}{msg:.2f}'.replace('.', ',')
+    return msg
+
+
+def resumo(valor, aumento=10, diminui=15):
+    print('--' * 20)
+    print(f'{"Resumo do valor":^40}')
+    print('--' * 20)
+    print(f'VALOR ANALISADO: {moeda(valor)}')
+    print(f'DOBRO: {dobro(valor, formato=True)}')
+    print(f'METADE: {metade(valor, True)}')
+    print(f'AUMENTO ({aumento}%): {aumentar(valor, aumento, True)}')
+    print(f'DIMINUIÇÃO ({diminui}%): {diminuir(valor, diminui, True)}')
+    print('--' * 20)
+
