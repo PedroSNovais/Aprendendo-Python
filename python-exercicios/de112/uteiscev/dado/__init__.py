94508a068732e5c160eef6f6a946e33df86302ee
Pedro Silva Novais <136607171+PedroSNovais@users.noreply.github.com>
2025-03-13 21:20:20 -0300

    desafio 112
    Deus é bom

--- python-exercicios/de112/uteiscev/dado/__init__.py
+++ python-exercicios/de112/uteiscev/dado/__init__.py
@@ -1,11 +1,13 @@
 def leiadinheiro(prompt):
     while True:
         valor = str(input(prompt))
+        valor2 = valor
         if '.' in valor:
-            valor = valor.replace('.', '')
-            print(valor)
-        if valor.isnumeric():
-            return float(valor)
+            valor2 = valor.replace('.', '')
+        if ',' in valor:
+            valor2 = valor.replace(',', '')
+        if valor2.isnumeric():
+            return float(valor.replace(',', '.'))
             break
         else:
             print(f'\033[1;31mERRO! O valor "{valor}" não foi aceito!\033[m')
