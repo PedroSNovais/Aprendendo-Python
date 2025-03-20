0da5dc4dbe40ef52a8bfb62366379c94c41c8768
Pedro Silva Novais <136607171+PedroSNovais@users.noreply.github.com>
2025-03-20 19:00:32 -0300

    Update uteis115.py

--- python-exercicios/de115/uteis115.py
+++ python-exercicios/de115/uteis115.py
@@ -1,19 +1,27 @@
 def dados():
    while True:
         try:
-            nome = str(input('Nome: ').strip().capitalize()).split()
-            snome = str(input('Sobrenome: ').strip().capitalize()).split()
+            nome = str(input('Nome: ').strip().capitalize())
             idade = int(input('Idade: '))
-            return {'nome': nome, 'idade': idade, 'snome': snome}
+            return {'nome': nome, 'idade': idade}
 
         except (TypeError, ValueError):
             print('\033[1;31mErro! Digite dados validos...\033[m')
 
 
 def continuar():
-    while True:
-        r = str(input('Quer continuar ? [S/N] ').strip().capitalize()[0])
-        if r in 'SN':
-            return r
-        else:
-            print('\033[1;31mErro! Digite "Sim" ou "Não"...\033[m')
+    try:
+        while True:
+            r = int(input('\033[1;33mSua Opção: \033[m'))
+            if r in (1, 2, 3):
+                return r
+            else:
+                print('\033[1;31mErro! Digite um número valido...\033[m')
+    except Exception:
+        print('\033[1;31mErro! Digite um valor valido...\033[m')
+
+
+def titulo(msg="MENU PRINCIPAL"):
+    print('-' * 40)
+    print(f'{msg:^40}')
+    print('-' * 40)
