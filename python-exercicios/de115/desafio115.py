966e7685401600ede857b93880f5aa492452bea8
Pedro Silva Novais <136607171+PedroSNovais@users.noreply.github.com>
2025-03-20 16:19:51 -0300

    Update desafio115.py

--- python-exercicios/de115/desafio115.py
+++ python-exercicios/de115/desafio115.py
@@ -1,17 +1,23 @@
 from uteis115 import *
 print(f'{" Desafio 115 ":*^40}')
+
+
+# recebendo dados
 pessoas = list()
 while True:
     p = dados()
-    resp = continuar()
+    # transformando em lista de escritas para printar (TEM COMO SIMPLIFICAR MAS EU TENHO PREGUIÇA)
     if len(p['snome']) == 0:
-        pessoas.append(f'{p["nome"][0]:<10} {p["idade"]} anos')
+        pessoas.append(f'{p["nome"][0]} {p["idade"]:>16} anos')
     else:
-        pessoas.append(f'{p["nome"][0]} {p["snome"][p["lsn"]]} {p["idade"]:>10} anos')
+        pessoas.append(f'{p["nome"][0]} {p["snome"][len(p["snome"])-1]} {p["idade"]:>10} anos')
+    # continuar ?
+    resp = continuar()
     if resp == 'N':
         break
     else:
         print("-"*30)
+# printando dados na tela
 print("-=" * 20)
 for i, e in enumerate(pessoas):
     print(f'{i:<3}', e)
