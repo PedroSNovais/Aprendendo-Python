1edcb5372f674a4eb3bea9fed228baa1b9a47f3b
PedroSNovais <Pedrosilvanovais7@gmail.com>
2026-01-09 16:07:35 -0300

    fix(OBI): deixa o exercicio do beecrowd 1019 mais simples

--- OBI/beecrowd/1019 -conversão-de-tempo.py
+++ OBI/beecrowd/1019 -conversão-de-tempo.py
@@ -6,11 +6,4 @@ hora = minutos // 60
 segundos = segundos % 60
 minutos = minutos % 60
 
-horario = [hora, minutos, segundos]
-cont = 0
-for i in horario:
-    if cont == 2:
-        print(i)
-    else:
-        print(f'{i}:', end = '')
-    cont += 1
+print(f'{hora}:{minutos}:{segundos}')
