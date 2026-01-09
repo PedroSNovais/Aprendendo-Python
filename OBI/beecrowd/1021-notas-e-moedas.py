b2c4823e8ecf2ab267bb999b3995e545531bfbe1
PedroSNovais <Pedrosilvanovais7@gmail.com>
2026-01-09 17:21:09 -0300

    fix(OBI): corrigi um problema de precisão do ponto flutuante no exercicio 1021

--- OBI/beecrowd/1021-notas-e-moedas.py
+++ OBI/beecrowd/1021-notas-e-moedas.py
@@ -1,15 +1,17 @@
 valor = float(input())
 
-notas = [100, 50, 20, 10, 5, 2]
+valor = valor * 100
+
+notas = [10000, 5000, 2000, 1000, 500, 200]
 print("NOTAS:")
 for nota in notas:
     quantidade_nota = valor // nota
     valor = valor % nota
-    print(f'{quantidade_nota:.0f} nota(s) de R$ {nota:.2f}')
+    print(f'{quantidade_nota:.0f} nota(s) de R$ {nota/100:.2f}')
 
-moedas = [1, 0.50, 0.25, 0.10, 0.05, 0.01]
+moedas = [100, 50, 25, 10, 5, 1]
 print('MOEDAS:')
 for moeda in moedas:
     quantidade_moeda = valor // moeda
     valor = valor % moeda
-    print(f'{quantidade_moeda:.0f} moeda(s) de R$ {moeda:.2f}')
+    print(f'{quantidade_moeda:.0f} moeda(s) de R$ {moeda/100:.2f}')
