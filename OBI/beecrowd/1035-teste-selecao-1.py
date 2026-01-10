78a15c841471c2ec26c4178101a76a4fbff7f6f4
PedroSNovais <Pedrosilvanovais7@gmail.com>
2026-01-10 15:32:15 -0300

    fix(1035): edita o exercicio 1035 da beecrowd para adiquirir 100% de acerto

--- OBI/beecrowd/1035-teste-selecao-1.py
+++ OBI/beecrowd/1035-teste-selecao-1.py
@@ -1,14 +1,14 @@
 def teste_selecao(lista: list) -> str:
     if lista[0] % 2 != 0:
-        return "Valores não aceitos"
-    if lista[1] < lista[2]:
-        return "Valores não aceitos"
-    if lista[3] < lista[0]:
-        return "Valores não aceitos"
-    if (lista[2] + lista[3]) < (lista[0] + lista[1]):
-        return "Valores não aceitos"
-    if lista[2] < 0 and lista[3] < 0:
-        return "Valores não aceitos"
+        return "Valores nao aceitos"
+    if lista[1] <= lista[2]:
+        return "Valores nao aceitos"
+    if lista[3] <= lista[0]:
+        return "Valores nao aceitos"
+    if (lista[2] + lista[3]) <= (lista[0] + lista[1]):
+        return "Valores nao aceitos"
+    if (lista[2] < 0) or (lista[3] < 0):
+        return "Valores nao aceitos"
     return "Valores aceitos"
 
 n = map(int, input().split())
